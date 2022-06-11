5046106daad1996f004cf8c99c10294dc7fd76ee
IGOR EIKI FERREIRA KUBOTA <19.02466-5@maua.br>
2022-06-11 12:15:22 -0300

    T2 - funciona cors, get noticias

--- Microserviço1-Noticias/noticias/index.js
+++ Microserviço1-Noticias/noticias/index.js
@@ -2,21 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
-const noticias = {};
+const noticias = [];
 contador = 0;
+const cors = require("cors");
+app.use(cors());
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', "*");
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
+//     next();
+//   })
 
 app.get('/api/noticias', (req, res) => {
-    res.send(noticias);
+    res.json({noticias});
 });
+
 
 app.post('/api/noticias', (req, res) => {
     contador++;
     const { titulo, texto, autor, data } = req.body;
-    noticias[contador] = {
+    noticias.push ({
         contador, texto, titulo, autor, data
-    }
-    res.status(200).send(noticias[contador]);
+    });
+    res.status(200).json({noticias});
 });
+
+
 
 app.listen(3000, () => {
     console.log('Noticias. Porta 3000')
